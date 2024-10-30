91af230e588a218a42eef9176fbceede4aef5acd
VAGNER OLIVEIRA <wagneroliveiroficial@gmail.com>
2024-10-30 01:25:24 +0000

    fix: ccs gameover modification

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -9,12 +9,20 @@ const GameOver = () => {
     const [quizStates, dispatch] = useContext(QuizContext);
 
     return (
-        <div id='gameover'>
+        <div id="gameover">
             <h2>Fim de Jogo</h2>
-            <p>Pontuação: {quizStates.score}</p>
-            <p>Você acertou {quizStates.score} de {quizStates.questions.length} perguntas.</p>
+            <p className='pontos-container'>
+                Pontuação: <span className="pontos">{quizStates.score}</span>
+            </p>
+            <p className='pontos-container'>
+                Você acertou <span className="pontos">{quizStates.score}</span>{' '}
+                de <span className="pontos">{quizStates.questions.length}</span>{' '}
+                perguntas.
+            </p>
             <img src={WellDone} alt="Fim do Quiz" />
-            <button onClick={() => dispatch({ type: 'NEW_GAME' })}>Reiniciar</button>
+            <button onClick={() => dispatch({ type: 'NEW_GAME' })}>
+                Reiniciar
+            </button>
         </div>
     );
 };
